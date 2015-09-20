ce4c1700af941e96f9249f143f33c02048882717
Alex Ware <alex.ware@gmail.com>
2015-09-20 21:32:26 +0100

    Pruning to relevant tests
    Test that the go that is auto generated is actually valid and the main
    cases are covered.

--- generator/generator_test.go
+++ generator/generator_test.go
@@ -6,120 +6,64 @@ import (
 	"encoding/json"
 	"fmt"
 	"testing"
-
-	"go/parser"
-	"go/printer"
-	"go/token"
-
-	"github.com/jmcvetta/napping"
 )
 
-//TODO: Write test cases.
-func TestLeaf(t *testing.T) {
-	/*var b bytes.Buffer
-	w := bufio.NewWriter(&b)*/
-	var a pw
-	type leaf int
-	var x leaf
-	err := GetType(x, "x", a)
-	if err != nil {
-		t.Error(err)
-	}
-	//w.Flush()
-	//a := b.String()
-	//if a != "x generator.leaf\n" {
-	//		t.Error("Expecting 'x generator.leaf', got " + a)
-	//}
-
+func TestArray(t *testing.T) {
+	j := `{"array":[1,2,3,4,5]}`
+	testJSONString(j, t)
 }
 
-func TestWriter(t *testing.T) {
-	/*var b bytes.Buffer
-	w := bufio.NewWriter(&b)*/
-	var a pw
-	m := make(map[string]interface{})
-	m["something"] = "hello"
-	m["cat"] = 5
-	n := make(map[string]interface{})
-	n["a"] = "b"
-	m["a"] = n
-	err := WriteGo(m, "x", a)
-	if err != nil {
-		t.Error(err)
-	}
-	//w.Flush()
-	//a := b.String()
-	//if a != "x generator.leaf\n" {
-	//		t.Error("Expecting 'x generator.leaf', got " + a)
-	//}
-
-}
-
-//Ideally this should test some JSON that's stored here as a file.
-//Write the generated code to a file
-//And try and unmarshall against this.
-func TestComplexJSON(t *testing.T) {
-	var result map[string]interface{}
-	var b bytes.Buffer
-	w := bufio.NewWriter(&b)
-	url := "https://alexwa.re/somejson.json"
-	resp, err := napping.Get(url, nil, &result, nil)
-	if err != nil {
-		t.Error(err)
-	}
-	if resp.Status() == 200 {
-		w.Write([]byte("package main \ntype resp "))
-		err := GetType(result, "", w)
-		if err != nil {
-			t.Error(err)
-		}
-		w.Flush()
-		a := b.String()
-		//	e, err := parser.ParseExpr(a)
-		fset := token.NewFileSet()
-		e, err := parser.ParseFile(fset, "", a, parser.AllErrors)
-		if err != nil {
-			t.Errorf("Source code generated was not valid go. %s", err.Error())
-		}
-
-		var p pw
-		printer.Fprint(p, fset, e)
-	}
-}
 func TestEmptyArray(t *testing.T) {
-	byt := []byte(`{"empty":[]}`)
-
-	var m map[string]interface{}
-	if err := json.Unmarshal(byt, &m); err != nil {
-		panic(err)
-	}
-	var b bytes.Buffer
-	w := bufio.NewWriter(&b)
-	WriteGo(m, "x", w)
-	w.Flush()
-
-	fmt.Println(b.String())
-
+	j := `{"empty":[]}`
+	testJSONString(j, t)
 }
+
 func TestStruct(t *testing.T) {
-	m := make(map[string]interface{})
-	m["something"] = "hello"
-	m["cat"] = 5
-	n := make(map[string]interface{})
-	n["a"] = "b"
-	m["a"] = n
+	j := `{"fieldA":"a","fieldB":0}`
+	testJSONString(j, t)
+}
+func TestNullField(t *testing.T) {
 	var b bytes.Buffer
 	w := bufio.NewWriter(&b)
-	GetType(m, "x", w)
+	err := GetType(nil, "name", w)
+	if err != nil {
+		t.Error(err)
+	}
 	w.Flush()
-
-	fmt.Println(b.String())
+	if len(b.Bytes()) != 0 {
+		t.Errorf("Expected no written data, got: %s", b.String())
+	}
 
 }
+func testJSONString(j string, t *testing.T) {
+	fmt.Printf("From:\n%s\n", j)
 
-type pw int
+	m, err := unmarshallFromString(j)
+	if err != nil {
+		t.Error(err)
+	}
+	code, err := generateCode(m)
+	if err != nil {
+		t.Error(err)
+	}
+	fmt.Printf("The following go was generated: %s", code)
+}
 
-func (p pw) Write(b []byte) (n int, err error) {
-	fmt.Print(string(b))
-	return len(b), nil
+func generateCode(input map[string]interface{}) (string, error) {
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	err := WriteGo(input, "x", w)
+	if err != nil {
+		return "", err
+	}
+	w.Flush()
+	return b.String(), nil
+}
+
+func unmarshallFromString(input string) (map[string]interface{}, error) {
+	byt := []byte(input)
+	var m map[string]interface{}
+	err := json.Unmarshal(byt, &m)
+	return m, err
+
 }
